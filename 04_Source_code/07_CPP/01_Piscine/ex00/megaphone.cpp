cb312ed85ff49b422eb0d3cde0ca8327b41342ad
Sergey Miroshnichenko <58503543+ommmirage@users.noreply.github.com>
2022-02-16 07:38:24 +0400

    Changed c++17 to c++03

--- 04_Source_code/07_CPP/01_Piscine/ex00/megaphone.cpp
+++ 04_Source_code/07_CPP/01_Piscine/ex00/megaphone.cpp
@@ -1,15 +1,43 @@
 #include <io.h>
 
+void capitalize(char* c)
+{
+    if (*c>=97 && *c<=122)
+        *c-=32;
+}
+
+void putSpace(int word, int* space, char c)
+{
+    if (word > 1 && !*space && c != ' ')
+        {
+            write(1, " ", 1);
+            *space = 0;
+        }
+}
+
 int main(int argc, char* argv[])
 {
     int fd = 1;
-    int i = 0;
-    char c;
-    while (argv[1][i])
+    if (argc == 1)
     {
-        c = argv[1][i];
-        
-        write(fd, &c, 1);
-        i++;
+        write(fd, "* LOUD AND UNBEARABLE FEEDBACK NOISE *", 38);
+    }
+    int word = 1;
+    int space = 0;
+    while (word < argc)
+    {
+        int i = 0;
+        char c;
+        putSpace(word, &space, argv[word][0]);
+        while (argv[word][i])
+        {
+            c = argv[word][i];
+            capitalize(&c);
+            write(fd, &c, 1);
+            i++;
+        }
+        if (c==' ')
+            space = 1;
+        word++;
     }
 }
